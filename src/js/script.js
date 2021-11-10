d833d2bc5fc35ee39cf7419bc4ea896be409b4b1
mikle <eaxample@gmail.com>
2021-11-10 20:12:19 +0300

    slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
-var name ="Mikle";
-let number =7;
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        /* autoplay: true,
+        autoplaySpeed: 4000, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/layers/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/layers/right.png"></button>',
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+            arrows: false, 
+            dots: true
+            }
+        
+             }
+            ]
+      });
+  });
+                  
