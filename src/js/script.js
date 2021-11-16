8bea3268e8c4f765d79c23797645cee4469551d5
mikle <eaxample@gmail.com>
2021-11-16 15:41:46 +0300

    slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,10 @@
-$(document).ready(function(){
+ $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         speed: 1200,
-        /* autoplay: true,
-        autoplaySpeed: 4000, */
+        autoplay: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/layers/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/layers/right.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/layers/right.png"></button>', 
         responsive: [
             {
             breakpoint: 992,
@@ -15,7 +14,6 @@ $(document).ready(function(){
             }
         
              }
-            ]
+            ] 
       });
   });
-                  
