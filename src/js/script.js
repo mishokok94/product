9874852b56b5d070e3fdfcc4f672dcedca66711f
mikle <eaxample@gmail.com>
2021-12-03 17:17:33 +0200

    validation form sintax

--- src/js/script.js
+++ src/js/script.js
@@ -78,4 +78,43 @@
       })
 
       });
+
+
+
+    function valideForms(form){
+      $(form).validate({
+        rules: {
+  
+              name : {
+                  required: true,
+                  minlength: 2
+                    },
+              phone : "required",
+              email : {
+                required: true,
+                email: true
+                      }
+                    },
+
+        messages: {
+              name: {
+                  required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("ВВедите {0} символа!")
+                },              
+              phone: "Пожалуйста, введите свой номер телефона",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адресс почты"
+                      }
+               }
+  
+      });
+
+    };
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-9999");
+   
     });
