3fb674933155053db6a694161c8c1bd0c7d23d1e
mikle <eaxample@gmail.com>
2021-12-03 14:11:57 +0200

    modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,20 @@
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
 
-  });
+      //modal
 
-  
+      $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function(i) {
+      $(this).on('click',function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+
+      });
+    });
