cb3ce18742b7376f4e5ebb98b4c98fda894c701c
mikle <eaxample@gmail.com>
2021-12-07 14:08:34 +0200

    php mail forms and animated scroll page

--- src/js/script.js
+++ src/js/script.js
@@ -116,5 +116,35 @@
     valideForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-9999");
-   
+    
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+      });
+      return false;
+  });
+
+  $(window).scroll(function(){
+      if($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      }
+      else{
+        $('.pageup').fadeOut();
+      }
+  });  
+
+  $("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html,body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
     });
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -1,5 +1,6 @@
 <?php 
 
+echo 'asdasd';die;
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
@@ -13,13 +14,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'mishokok94@gmail.com';                 // Наш логин
+$mail->Password = 'Radio2019';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('mishokok94@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('mishokok94@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
