ba60b2d75aaeb9426ab725a539047b8a925e1b98
mikle <eaxample@gmail.com>
2021-12-08 16:56:51 +0200

    validation verification

--- src/js/script.js
+++ src/js/script.js
@@ -141,10 +141,13 @@
         $('.pageup').fadeOut();
       }
   });  
+//Scroll animated
 
-  $("a[href^='#']").click(function(){
-    var _href = $(this).attr("href");
+
+  $("a[href^=#up]").click(function(){
+    const _href = $(this).attr("href");
     $("html,body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
   });
+    new WOW().init();
     });
